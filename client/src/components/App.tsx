7e2f37a65b5aafd66ebb9a5760803391f7761d54
mrochny <tpalinski@icloud.com>
2023-11-10 22:02:19 +0100

    LBL-11 Create sample frontpage

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1> LBLS </h1>
+      <img src='https://pg.edu.pl/files/styles/large/public/2021-06/pg_logo_kolor_podstawowa_2.jpg?itok=HJEBcvfI'/>
     </div>
   );
 }
